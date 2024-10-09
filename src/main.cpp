7cdef04c94b4bf40279a917bcc9d50b55b66b5b5
jonathan03ant <jonathan03ant@gmail.com>
2024-10-09 15:10:59 -0400

    fmwr srs code

--- src/main.cpp
+++ src/main.cpp
@@ -5,5 +5,6 @@
 		* Set any pin as input or output, except for default pins
 		* Set any pin as high or low(when in outputMode), except for default pins
 		* Configure pins for I2c and possibly SPI interfaces
+	* We will be using serial communication to send commands from a python script remotely
+	* Python --------> Talks with CPP code on ESP32
 */
-
