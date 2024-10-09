353e1db1d01f8a4f6c61335a526125a7fe37a742
jonathan03ant <jonathan03ant@gmail.com>
2024-10-09 13:58:54 -0400

    Main fmwr code added

---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+/*
+	* This will be a generic firmware for my ESP32-s3
+	* Plans of implementation currently are
+		* Query all GPIO pin statuses
+		* Set any pin as input or output, except for default pins
+		* Set any pin as high or low(when in outputMode), except for default pins
+		* Configure pins for I2c and possibly SPI interfaces
+*/
+
