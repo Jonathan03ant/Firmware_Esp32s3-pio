f33dad8ec0e21a5e2569758df09a155bd96ac55a
jonathan03ant <jonathan03ant@gmail.com>
2024-10-12 18:18:48 -0400

    frmwr #printing gpios

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,9 @@
-/*
-	* This will be a generic firmware for my ESP32-s3
-	* Plans of implementation currently are
-		* Query all GPIO pin statuses
-		* Set any pin as input or output, except for default pins
-		* Set any pin as high or low(when in outputMode), except for default pins
-		* Configure pins for I2c and possibly SPI interfaces
-	* We will be using serial communication to send commands from a python script remotely
-	* Python --------> Talks with CPP code on ESP32
-*/
-#include <Arduino.h>
-#include "pin_config.h"
-
-#define DEBUG 1 
-
-#if DEBUG == 1
-#define debug(x) Serial.print(x)
-#define debugln(x) Serial.println(x)
-#else
-#define debug(x)
-#define debugln(x)
-#endif 
-
 #include "pin_config.h"  // Include your pin configuration file
 #include <Arduino.h>
 
+/*
+    * Returns GPIO Index in the list
+*/
 int findPinIndex(int gpioNumber) {
     int totalPins = getTotalPins();
     for (int i = 0; i < totalPins; i++) {
@@ -35,32 +15,32 @@ int findPinIndex(int gpioNumber) {
 }
  
 void initializePins() {
-    Serial.println("Initializing Pins...");
-    for (int gpioNum = 0; gpioNum <= 52; gpioNum++) {
-        int index = findPinIndex(gpioNum);
-        if (index == -1) {
-            Serial.print("Skipping Pin ");
-            Serial.println(gpioNum);
-            continue;
-        }
-        Pin &pin = esp32s3Pins[index];
-        Serial.print("Initializing Pin ");
+    Serial.println(F("Initializing Pins..."));
+    int totalPins = getTotalPins();
+    Serial.print(F("Total Pins: "));
+    Serial.println(totalPins);
+
+    for (int i = 0; i < totalPins; i++) {
+        Serial.print(F("Processing index: "));
+        Serial.println(i);
+
+        Pin &pin = esp32s3Pins[i];
+        Serial.print(F("Pin Number: "));
         Serial.println(pin.GPIO_NUMBER);
-        // Perform initialization based on pin.mode and pin.state
-        if (pin.mode == "INPUT") {
-            pinMode(pin.GPIO_NUMBER, INPUT);
-        } else if (pin.mode == "OUTPUT") {
-            pinMode(pin.GPIO_NUMBER, OUTPUT);
-            digitalWrite(pin.GPIO_NUMBER, (pin.state == "HIGH") ? HIGH : LOW);
+        Serial.print(F("Pin Type: "));
+        Serial.println(pin.type);
+
+        if (strcmp(pin.type, "GPIO") == 0) {
+            // Existing code...
+        } else {
+            Serial.print(F("Skipping strapping/non-GPIO Pin "));
+            Serial.print(pin.GPIO_NUMBER);
+            Serial.print(F(" (Type: "));
+            Serial.print(pin.type);
+            Serial.println(F(")"));
         }
-        Serial.print("Pin ");
-        Serial.print(pin.GPIO_NUMBER);
-        Serial.print(" initialized with state: ");
-        Serial.println(pin.state);
     }
 }
-
-
 
 void setup() {
   Serial.begin(115200); // or keep it at 460800 if it is required
